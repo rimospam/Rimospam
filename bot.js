12e7be42f21f791e1d1ce7e07a191ead427efff5
rimospam <49791552+rimospam@users.noreply.github.com>
2019-04-22 18:08:48 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("569069161859252236")
+setInterval(function() {
+channel.send(`Spam For Credit`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
